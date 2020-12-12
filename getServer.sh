c95b0649706d9a3100ba63a6eec9338eae7e6db8
dumbo25 <dumbo25@users.noreply.github.com>
2020-12-12 13:22:38 -0600

    Update getServer.sh

--- getServer.sh
+++ getServer.sh
@@ -5,7 +5,7 @@
 # git clone seems to require my username and password
 cd ~/.
 mkdir api
-cd api
+cd ~/api/.
 
 wget https://raw.githubusercontent.com/dumbo25/restful_api/master/README.md
 wget https://github.com/dumbo25/restful_api/blob/master/favicon.png
@@ -15,55 +15,55 @@ wget https://raw.githubusercontent.com/dumbo25/restful_api/master/rest_server.py
 wget https://raw.githubusercontent.com/dumbo25/restful_api/master/server_cron.sh
 
 mkdir cpu
-cd cpu
+cd ~/api/cpu/.
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/cpu/get.backup
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/cpu/get.json
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/cpu/get.py
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/cpu/get.txt
 
-cd ..
+cd ~/api
 mkdir disk
-cd disk
+cd ~/api/disk/.
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/disk/get.backup
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/disk/get.json
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/disk/get.py
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/disk/get.txt
 
-cd ..
+cd ~/api
 mkdir memory
-cd memory
+cd ~/api/memory/.
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/memory/get.backup
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/memory/get.json
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/memory/get.py
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/memory/get.txt
 
-cd ..
+cd ~/api
 mkdir os
-cd os
+cd ~/api/os/.
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/os/get.backup
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/os/get.json
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/os/get.py
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/os/get.txt
 
-cd ..
+cd ~/api
 mkdir reboot
-cd reboot
+cd ~/api/reboot/.
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/reboot/get.backup
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/reboot/get.json
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/reboot/get.py
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/reboot/get.txt
 
-cd ..
+cd ~/api
 mkdir time
-cd time
+cd ~/api/time/.
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/time/get.backup
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/time/get.json
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/time/get.py
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/time/get.txt
 
-cd ..
+cd ~/api
 mkdir uptime
-cd uptime
+cd ~/api/uptime/.
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/uptime/get.backup
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/uptime/get.json
 https://raw.githubusercontent.com/dumbo25/restful_api/master/server/uptime/get.py
